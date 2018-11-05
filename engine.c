7c6acf50171bfd6a6d523e45a1691e61c38ae1e2
fenix <betzy_skamm@hotmail.com>
2018-11-04 22:51:54 -0800

    config sdl

--- engine.c
+++ engine.c
@@ -1,7 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <SDL/SDL.h>
+
+#undef main
+#define W 640
+#define H 480
+
+static SDL_Surface *surface = NULL;
 
 int main()
 {
-    printf("Hello World\n");
+    surface = SDL_SetVideoMode(W,H,32,0);
+
+    SDL_EnableKeyRepeat(150, 30);
+    SDL_ShowCursor(SDL_DISABLE);
+
+    for(;;)
+    {
+        SDL_LockSurface(surface);
+        SDL_UnlockSurface(surface);
+        SDL_Flip(surface);
+        SDL_Event event;
+        while(SDL_PollEvent(&event))
+        {
+            switch (event.type)
+            {
+                case SDL_KEYDOWN:
+                case SDL_KEYUP:
+                    switch(event.key.keysym.sym)
+                    {
+                        case 'q': goto done;
+                    }
+                break;
+                case SDL_QUIT:
+                    goto done;
+            }
+        }
+    }
+done:
+    SDL_Quit();
     return 0;
 }
